a1480fee015f1f31c7ba9c45def0443124ab2911
Yaroslav Shalenyk <yshalenyk@quinatagroup.org>
2017-09-07 19:44:45 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(name='openprocurement.auction.insider-js',
+      version='0.0.2a2+eauctions',
+      description='frontend application for openprocurement.auction',
+      classifiers=[
+          "Framework :: Pylons",
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application"
+      ],
+      keywords="web services",
+      author='Quintagroup, Ltd.',
+      author_email='info@quintagroup.com',
+      license='Apache License 2.0',
+      url='https://github.com/openprocurement/openprocurement.auction.insider-js',
+      include_package_data=True,
+      zip_safe=False,
+)
