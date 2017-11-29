f07edff6fe32aa2371637b1f8f579c06cbe6b96e
Yuriy Hvozdovych <gvizdyk@quintagroup.com>
2017-11-29 18:54:18 +0200

    Resolved a490729281641748. Updated view amount.

--- src/app/filters/FormatNumber.js
+++ src/app/filters/FormatNumber.js
@@ -1,5 +1,17 @@
-angular.module('auction').filter('formatnumber', ['$filter', function ($filter) {
+angular.module('auction').filter('formatnumber', ['$filter', function($filter) {
   return function(val) {
+    var isInteger = function(num) {
+      return (num ^ 0) === num;
+    };
+    var format_number = function(val) {
+      return math.format(Number(val), {
+        notation: 'fixed',
+        precision: 2
+      }).replace(/(\d)(?=(\d{3})+\.)/g, "$1'").replace(/\./g, ",");
+    };
+    if (isInteger(val)) {
+      return format_number(math.eval(math.format(math.fraction(val))).toFixed(2));
+    }
     return ($filter('number')(val) || "").replace(/,/g, "'") || "";
   };
 }]);
--- src/app/filters/Fraction.js
+++ src/app/filters/Fraction.js
@@ -4,9 +4,15 @@ angular.module('auction').filter('fraction', [function () {
       return math.format(Number(val), {
         notation: 'fixed',
         precision: 2
-      }).replace(/(\d)(?=(\d{3})+\.)/g, "$1'").replace(/\./g, ",");
+      }).replace(/(\d)(?=(\d{3})+\.)/g, '$1 ').replace(/\./g, ",");
     };
     if (!angular.isUndefined(val)) {
+      if (angular.isNumber(val)){
+        return format_function(val);
+      }
+      if (coeficient) {
+        return format_function(math.eval(math.format(math.fraction(val) * math.fraction(coeficient))).toFixed(2));
+      }
       return format_function(math.eval(math.format(math.fraction(val))).toFixed(2));
     }
     return "";
