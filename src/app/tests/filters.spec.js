786bd3ea652ab82ade6a1907111bf451345beb25
Yuriy Hvozdovych <gvizdyk@quintagroup.com>
2017-12-01 17:10:58 +0200

    updated filters.spec.js

--- src/app/tests/filters.spec.js
+++ src/app/tests/filters.spec.js
@@ -3,28 +3,28 @@ beforeEach(module('auction'));
 
 describe('Unit: Testing Filter "formatnumber" - ', function() {
   it('formatnumber should format positive numbers', inject(function($filter) {
-    expect($filter('formatnumber')(100000)).toEqual('100 000');
-    expect($filter('formatnumber')(10000)).toEqual('10 000');
-    expect($filter('formatnumber')(1000)).toEqual('1 000');
-    expect($filter('formatnumber')(100)).toEqual('100');
-    expect($filter('formatnumber')(10)).toEqual('10');
-    expect($filter('formatnumber')(1)).toEqual('1');
+    expect($filter('formatnumber')(100000)).toEqual('100\'000.00');
+    expect($filter('formatnumber')(10000)).toEqual('10\'000.00');
+    expect($filter('formatnumber')(1000)).toEqual('1\'000.00');
+    expect($filter('formatnumber')(100)).toEqual('100.00');
+    expect($filter('formatnumber')(10)).toEqual('10.00');
+    expect($filter('formatnumber')(1)).toEqual('1.00');
   }));
 
   it('formatnumber should format nagative numbers', inject(function($filter) {
-    expect($filter('formatnumber')(-100000)).toEqual('-100 000');
-    expect($filter('formatnumber')(-10000)).toEqual('-10 000');
-    expect($filter('formatnumber')(-1000)).toEqual('-1 000');
-    expect($filter('formatnumber')(-100)).toEqual('-100');
-    expect($filter('formatnumber')(-10)).toEqual('-10');
-    expect($filter('formatnumber')(-1)).toEqual('-1');
+    expect($filter('formatnumber')(-100000)).toEqual('-100\'000.00');
+    expect($filter('formatnumber')(-10000)).toEqual('-10\'000.00');
+    expect($filter('formatnumber')(-1000)).toEqual('-1\'000.00');
+    expect($filter('formatnumber')(-100)).toEqual('-100.00');
+    expect($filter('formatnumber')(-10)).toEqual('-10.00');
+    expect($filter('formatnumber')(-1)).toEqual('-1.00');
   }));
 
   it('formatnumber should works with incorect values', inject(function($filter) {
     expect($filter('formatnumber')('string')).toEqual('');
-    expect($filter('formatnumber')('')).toEqual($filter('number')('')); // '0' 
+    expect($filter('formatnumber')('')).toEqual($filter('number')('')); // '0'
     expect($filter('formatnumber')({})).toEqual('');
-    expect($filter('formatnumber')(true)).toEqual('1');
-    expect($filter('formatnumber')(false)).toEqual('0');
+    expect($filter('formatnumber')(true)).toEqual('');
+    expect($filter('formatnumber')(false)).toEqual('');
   }));
 });
